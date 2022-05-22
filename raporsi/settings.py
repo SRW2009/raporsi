c26b73f8cc9102fbfad5caceca661a53d8d23a5d
Anvarisy <anvarisy@gmail.com>
2022-05-22 10:24:31 +0700

    Add docker

--- raporsi/settings.py
+++ raporsi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_j6o%9-^p1tkehs50)-lhw*h$&*9yq)zd)stbxi#(cm2kfuv$)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 if DEBUG:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['*']
 else:
     ALLOWED_HOSTS = ['restful-rapor.sekolahimpian.com']
 
@@ -81,10 +81,10 @@ if DEBUG:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'rapor',
-        'USER':'si',
-        'PASSWORD':'sekolahimpian',
-        'HOST': 'localhost',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
         "ATOMIC_REQUESTS": True,
         'PORT': 5432,
     }
